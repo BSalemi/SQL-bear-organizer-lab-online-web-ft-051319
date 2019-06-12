dfbcd6e76bd1988fc993ba7bbc1bb095bf8a6917
Brian Salemi <briandsalemi@gmail.com>
2019-06-12 13:06:09 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -13,8 +13,3 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (5,"Meli
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (6,"Grinch", 2, "M", "Black", "Grinchy", 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (7,"Wendy", 6, "F", "Blue", "naive", 1);
-
-
-
-
-)
